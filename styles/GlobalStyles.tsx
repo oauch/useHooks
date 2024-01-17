7b6fa5030fd297f78ab3ce2d4e580274f1f0728e
oauch <lgm9515@naver.com>
2024-01-17 18:45:00 +0900

    [#5] Feat: GlobalStyles 추가

---
+++ styles/GlobalStyles.tsx
@@ -0,0 +1,9 @@
+import { reset } from "@/styles/reset";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+`;
+
+export default GlobalStyles;
