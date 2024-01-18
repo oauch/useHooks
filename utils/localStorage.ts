1bc9ea7dc39f33158dd49a38a132c033388f0d7d
oauch <lgm9515@naver.com>
2024-01-18 14:43:29 +0900

    [#7] Feat: removeLocalStorage 추가

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -4,6 +4,9 @@ export const getLocalStorage = (key: string) => {
 export const setLocalStorage = (key: string, value: string) => {
   if (typeof window !== "undefined") localStorage.setItem(key, value);
 };
+export const removeLocalStorage = (key: string) => {
+  if (typeof window !== "undefined") localStorage.removeItem(key);
+};
 export const clearLocalStorage = () => {
   if (typeof window !== "undefined") localStorage.clear();
 };
