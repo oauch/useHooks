cd82feb78fe2fbdc4634174c8d15d49771e2a1ca
oauch <lgm9515@naver.com>
2024-03-22 17:08:47 +0900

    feat: Button 컴포넌트 size 기본값

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -12,24 +12,28 @@ function Button({ action, children, ...props }: ButtonProps) {
 export default Button;
 
 const Wrapper = styled.button<ButtonProps>`
-  width: ${(props) => {
-    switch (props.size) {
+  width: ${({ size }) => {
+    switch (size) {
       case "sm":
         return 100;
       case "md":
         return 150;
       case "lg":
         return 200;
+      default:
+        return 100;
     }
   }}px;
-  height: ${(props) => {
-    switch (props.size) {
+  height: ${({ size }) => {
+    switch (size) {
       case "sm":
         return 30;
       case "md":
         return 35;
       case "lg":
         return 40;
+      default:
+        return 30;
     }
   }}px;
   color: ${({ color }) => color};
