343c7aff1fdbaf2b2da6cea9481a23087d43558f
oauch <lgm9515@naver.com>
2024-03-22 16:14:20 +0900

    feat: emotion css 선택자 할 수 있도록 허용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    emotion: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
